f1389e9b0cc54a6ac9cd3b7a851f46cfe85090e4
Alice <lisa03111@mail.ru>
2019-12-31 17:10:38 +0300

    two_commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("burger").onclick = function(){
+        openCloseMenu()
+};
+
+function openCloseMenu(){
+    document.getElementById("menu").classList.toggle("open");
+}
+
+
